020224a4cc3e51194ef2a88395d3325b71693b8a
carolineaire <noblet.caroline.pro@gmail.com>
2026-02-24 15:00:43 +0100

    feat: update SortieController to fetch and display all sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\SortiesRepository;
 
 final class SortieController extends AbstractController
 {
     #[Route('/', name: 'app_sortie')]
-    public function index(): Response
+    public function index(SortiesRepository $repo): Response
     {
+        $sorties = $repo->findAll();
         return $this->render('sortie/sortie.html.twig', [
-            'controller_name' => 'SortieController',
+            'sortie' => $sorties,
         ]);
     }
 }
