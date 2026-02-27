61c71675e80751d91173dfc814657a640c221b9b
lbourcier2024 <bourcier.lukas44@gmail.com>
2026-02-27 14:51:54 +0100

    add forgot password

--- src/Controller/ForgotController.php
+++ src/Controller/ForgotController.php
@@ -2,23 +2,62 @@
 
 namespace App\Controller;
 
-use App\Entity\Lieux;
-use App\Form\LieuxType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Form\ForgotFormType;
+use App\Service\ProfilService;
 
 final class ForgotController extends AbstractController
 {
     #[Route('/forgot', name: 'password_forgot')]
-    public function forgot(Request $request, EntityManagerInterface $em): Response {
-        $form = $this->createForm(RegistrationFormType::class, $participant);
+    public function forgot(
+        Request $request, 
+        EntityManagerInterface $em, 
+        ProfilService $profilService,
+        UserPasswordHasherInterface $userPasswordHasher
+    ): Response {
+        $form = $this->createForm(ForgotFormType::class);
         $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer les données du formulaire
+            $data = $form->getData();
+            $email = $data['email'] ?? null;
+            $telephone = $data['telephone'] ?? null;
+            
+            if ($email && $telephone) {
+                // Récupérer l'utilisateur
+                $user = $profilService->getParticipantsWithMailAndNumberPhone($email, $telephone);
+
+                if ($user) {
+                    // Réinitialiser le mot de passe à "1234567"
+                    $hashedPassword = $userPasswordHasher->hashPassword($user, "1234567");
+                    $user->setPassword($hashedPassword);
+                    
+                    // Sauvegarder en base de données
+                    $em->persist($user);
+                    $em->flush();
+                    
+                    // Message de succès
+                    $this->addFlash('success', 'Votre mot de passe a été réinitialisé à 1234567');
+                } else {
+                    // Message d'erreur
+                    $this->addFlash('error', 'Aucun utilisateur trouvé avec ces informations');
+                }
+            } else {
+                $this->addFlash('error', 'Email et téléphone sont requis');
+            }
+            
+            // Redirection
+            return $this->redirectToRoute('password_forgot');
+        }
+
         return $this->render('security/forgot.html.twig', [
+            'forgotForm' => $form,
         ]);
     }
 }
--- src/Form/ForgotFormType.php
+++ src/Form/ForgotFormType.php
@@ -2,21 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Participants;
-
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ForgotFormType extends AbstractType
 {
@@ -24,19 +14,36 @@ class ForgotFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-
+                'label' => 'Email',
                 'required' => true,
+                'attr' => [
+                    'class' => 'form-input',
+                    'placeholder' => 'votre@email.com'
+                ]
             ])
-            ->add('telephone', TelType::class, [
-
+            ->add('telephone', TextType::class, [
+                'label' => 'Numéro de téléphone',
                 'required' => true,
-            ]);
+                'attr' => [
+                    'class' => 'form-input',
+                    'placeholder' => '0612345678'
+                ]
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Participants::class,
+            'data_class' => null,  // Important: pas de classe d'entité
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'forgot_form',
         ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'forgot_form';  // Important: ce préfixe doit correspondre
     }
 }
